51db242fc60817aad9d80f664045b6774726741a
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-17 12:22:58 +0700

    in ra cho dễ nhìn

--- gym_splendor/envs/splendor.py
+++ gym_splendor/envs/splendor.py
@@ -73,7 +73,7 @@ class SplendporEnv(gym.Env):
         self.board.hien_the()
         t = 0
         for p in self.player:
-            print(p.name,p.score,list(p.stocks.values()),list(p.stocks_const.values()),end="")
+            print('\n',p.name,p.score,list(p.stocks.values()),list(p.stocks_const.values()),end="")
             print(" Card got: ",end="")
             for i in p.card_open:
                 print(i.id, end=" ")
