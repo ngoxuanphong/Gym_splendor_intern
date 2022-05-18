26d5a9256bfe9829bdc5d37b135c47a03cc47561
longle181599 <long.ln181599@gmail.com>
2022-05-18 14:51:07 +0700

    add longlee

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -6,7 +6,7 @@
 from gym_splendor.envs.agents import Trang as a1
 from gym_splendor.envs.agents import Trang as a2
 from gym_splendor.envs.agents import Dat as a3
-from gym_splendor.envs.agents import Dat as a4
+from gym_splendor.envs.agents import longlee as a4
 
-ListPlayer = [a1.Agent("Trang"),a2.Agent("Trang2"),a3.Agent("Dat"),a4.Agent("Dat2")]
+ListPlayer = [a1.Agent("Trang"),a2.Agent("Trang2"),a3.Agent("Dat"),a4.Agent("longlee")]
 # 
