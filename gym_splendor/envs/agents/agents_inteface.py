505c6138970ec80714adc7df8ef3b04b1a130546
tranquocnamphi <tranquocnamphi@gmail.com>
2022-05-21 22:18:07 +0700

    add final

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -1,7 +1,7 @@
 from gym_splendor.envs.agents import agent_2 as a1
-from gym_splendor.envs.agents import agent_2 as a2
-from gym_splendor.envs.agents import agent_2 as a3
-from gym_splendor.envs.agents import agent_2 as a4
+from gym_splendor.envs.agents import random_final as a2
+from gym_splendor.envs.agents import random_final as a3
+from gym_splendor.envs.agents import random_final as a4
 
 # from gym_splendor.envs.agents import agent_save as a1
 # from gym_splendor.envs.agents import agent_save as a2
