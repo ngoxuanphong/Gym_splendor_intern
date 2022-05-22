4f76dab1c8f4d571347a1aae98b3fc09f2603347
Vietanh <anhpv2710@gmail.com>
2022-05-22 13:54:21 +0700

    hahahah

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -1,12 +1,11 @@
 from gym_splendor.envs.agents import agent_random as a1
 from gym_splendor.envs.agents import agent_random as a2
-from gym_splendor.envs.agents import agent_random as a3
-from gym_splendor.envs.agents import agent_random as a4
+from gym_splendor.envs.agents import agent_2 as a3
+from gym_splendor.envs.agents import Agent_mine as a4
 
 # from gym_splendor.envs.agents import agent_save as a1
 # from gym_splendor.envs.agents import agent_save as a2
 # from gym_splendor.envs.agents import agent_save as a3
 # from gym_splendor.envs.agents import agent_save as a4
 
-ListPlayer = [a1.Agent("Vang"),a2.Agent("Hưng"),a3.Agent("NA"),a4.Agent("Thơm")]
-# 
+ListPlayer = [a1.Agent("Vang"),a2.Agent("Hưng"),a3.Agent("NA"),a4.Agent("VA")]
--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     env = gym.make('gym_splendor-v0')
     env.reset()
     start_time = time.time()
-    while env.turn <1000:
+    while env.turn <50:
         o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(state = env.state))
         env.render()
         if done == True:
