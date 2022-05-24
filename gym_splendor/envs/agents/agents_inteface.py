04200cfc75e3743bfdc5edc8f12b8274e0e0f9fa
longle181599 <long.ln181599@gmail.com>
2022-05-24 09:03:29 +0700

    use Long1 to win bot random

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -6,8 +6,8 @@
 from gym_splendor.envs.agents import random_final as a1
 from gym_splendor.envs.agents import random_final as a2
 from gym_splendor.envs.agents import random_final as a3
-from gym_splendor.envs.agents import random_final as a4
+from gym_splendor.envs.agents import Long as a4
 from gym_splendor.envs.agents import Long1 as a5
 
 # ListPlayer = [a1.Agent("Trang"),a2.Agent("Trang2"),a3.Agent("Dat"),a4.Agent("longlee")]
-ListPlayer = [a1.Agent("final1"),a2.Agent("final2"),a3.Agent("final3"),a5.Agent("Long")]
+ListPlayer = [a1.Agent("final1"),a2.Agent("final2"),a3.Agent("final3"),a4.Agent("Long")]
