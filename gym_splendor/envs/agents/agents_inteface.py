692e1ce10e6301cbed7a738e8f95fd58b109df96
QuynhTrangDao <quynhtrang.vis@gmail.com>
2022-05-26 16:59:48 +0700

    update 26/05

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -1,12 +1,12 @@
-from gym_splendor.envs.agents import Trangold as a1
-from gym_splendor.envs.agents import Trangold as a2
-from gym_splendor.envs.agents import Trangold as a3
-from gym_splendor.envs.agents import Trangold as a4
+from gym_splendor.envs.agents import agent_P as a1
+from gym_splendor.envs.agents import agent_Dat as a2
+from gym_splendor.envs.agents import agent_MA as a3
+from gym_splendor.envs.agents import An_Random as a4
 
 # from gym_splendor.envs.agents import agent_save as a1
 # from gym_splendor.envs.agents import agent_save as a2
 # from gym_splendor.envs.agents import agent_save as a3
 # from gym_splendor.envs.agents import agent_save as a4
 
-ListPlayer = [a1.Agent("Trang_xinh01"),a2.Agent("Trang_xinh02"),a3.Agent("Hoang01"),a4.Agent("Hoang02")]
+ListPlayer = [a1.Agent("Trang_xinh01"),a2.Agent("Dat"),a3.Agent("Hoang01"),a4.Agent("Hoang02")]
 # 
